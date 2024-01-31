27b84929ce1a4be08c49a3283fd54f6aa9a71d55
kost90 <kostdonc90@gmail.com>
2024-01-31 11:19:35 +0000

    created project, created css config, added font

---
+++ allplasticworld/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-brightBlue">All Plastic world!</h1>
+      <h2>Its h2 tag</h2>
+      <h3>Its h3 tag</h3>
+      <p>Its p tag</p>
+      <a href="#">Its link</a>
+    </>
+  );
+}
+
+export default App;
---
+++ allplasticworld/tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        brightBlue: "#01C3FD",
+      },
+      backgroundColor: {
+        body: "#000618",
+        cardBg:"rgba(40, 100, 151, 0.27)",
+      },
+      zIndex: {
+        blurZindex: "-1",
+      },
+      blur: {
+        blurBg: "100px",
+      },
+      backgroundImage: {
+        blur1:
+          "linear-gradient(138deg, #0194FE 30.62%, rgba(1, 148, 254, 0.00) 62.8%)",
+        btnBg: "linear-gradient(66deg, #00C3FD 25.98%, #0194FE 65.48%)",
+      },
+      fontSize: {
+        h1Desk: "48px",
+        h1Mob: "36px",
+        h2Desk: "30px",
+        h2Mob: "18px",
+        h3Mob: "13px",
+        h3Desk: "25px",
+        pDesk: "18px",
+        pMob: "13px",
+        link:"16px",
+      },
+      borderColor:{
+        btnBorderCol:"#3DBDF1",
+      },
+    },
+  },
+  plugins: [],
+};
